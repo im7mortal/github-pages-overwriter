38f1ab06a6db40a60748a259200d22cdc02e2585
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-17 13:02:54 -0400

    dev files

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install act
+# curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+
+REPO_ABSOLUTE_PATH=$(dirname "$(readlink -f "$0")")
+
+act --local-repository "dev/github-pages-overwriter@v0=${REPO_ABSOLUTE_PATH}" --secret-file "${SECRETS_PATH}"
