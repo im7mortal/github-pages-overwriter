fc4ef7bc2be0a5454865cc2939efac285ac6a62a
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-24 20:51:25 -0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@
 # curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
 REPO_ABSOLUTE_PATH=$(dirname "$(readlink -f "$0")")
+SECRETS_PATH="../.secrets"
 
 act --local-repository "dev/github-pages-overwriter@v0=${REPO_ABSOLUTE_PATH}" --secret-file "${SECRETS_PATH}"
