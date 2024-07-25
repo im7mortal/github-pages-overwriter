bbd2123af313be1ac46441a1b61c21f43ac5be8b
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-24 22:21:25 -0400

    move integration tests under helper action

--- run.sh
+++ run.sh
@@ -6,4 +6,7 @@
 REPO_ABSOLUTE_PATH=$(dirname "$(readlink -f "$0")")
 SECRETS_PATH="../.secrets"
 
-act --local-repository "dev/github-pages-overwriter@v0=${REPO_ABSOLUTE_PATH}" --secret-file "${SECRETS_PATH}"
+act \
+  --local-repository "dev/github-pages-overwriter@v0=${REPO_ABSOLUTE_PATH}" \
+  --local-repository "dev/helper@v0=${REPO_ABSOLUTE_PATH}/helper" \
+  --secret-file "${SECRETS_PATH}"
